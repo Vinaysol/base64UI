253626cc2a5ec03de23e7a11a7120a777425a1e2
vinaysolanki <vinaysolanki@hotmail.com>
2022-07-23 04:42:45 +0530

    working app

--- src/main/java/net/sourceforge/base64de/main/Main.java
+++ src/main/java/net/sourceforge/base64de/main/Main.java
@@ -1,17 +1,12 @@
 package net.sourceforge.base64de.main;
 
-import net.sourceforge.base64de.ui.AppLayout;
 import net.sourceforge.base64de.ui.UiLayout;
-
-import java.awt.*;
 
 
 public class Main
 {
-
-        public static void main(String args[])
+     public static void main(String args[])
         {
-            AppLayout appLayout=new AppLayout();
             UiLayout ui=new UiLayout();
             ui.appLayout();
         }
